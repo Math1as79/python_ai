e1a7004d1f2f44aef34d8193febeb122a635ca57
Math1as79 <135426727+Math1as79@users.noreply.github.com>
2024-01-24 19:17:23 +0100

    User input check

--- Lab1/intro.py
+++ Lab1/intro.py
@@ -13,23 +13,27 @@ We offer the following games:
     - Black Jack
 
         """
+games = ["H", "M", "R", "B"]
 
 cprint(welcome, "green")
-play = True
-while play == True:
-    game = input("Choose a game(H/M/R/B): ")
 
-    if game == "H":
-        subprocess.run(["python", "hangman.py"])
-    elif game == "M":
-        subprocess.run(["python", "memory.py"])
-    elif game == "R":
-        subprocess.run(["python", "rock_paper_scissors.py"])
-    elif game == "B":
-        subprocess.run(["python", "blackjack.py"])
+while True:
+    game = input("Choose a game(H/M/R/B): ").upper()
+    
+    if game in games:
+        if game == "H":
+            subprocess.run(["python", "hangman.py"])
+        elif game == "M":
+            subprocess.run(["python", "memory.py"])
+        elif game == "R":
+            subprocess.run(["python", "rock_paper_scissors.py"])
+        elif game == "B":
+            subprocess.run(["python", "blackjack.py"])
 
-    play_again = input("Play another game? (Y/N): ")
-    if play_again == "N":
-        break
+        play_again = input("Play another game? (Y/N): ")
+        if play_again == "N":
+            break
+    else:
+        cprint("That game doesn't exists yet...","red")
 
 cprint("Thank you for playing.", "light_blue")
